a9beb4fe1dd33ba496906075089c477281df905d
Ikechi Michael <mykehell123@gmail.com>
2018-04-14 19:10:10 +0100

    feat: add dotnet env plus attempt send sms in test

--- Jusibe.Tests/SmsTest.cs
+++ Jusibe.Tests/SmsTest.cs
@@ -3,18 +3,29 @@ using Xunit;
 using Jusibe;
 using Jusibe.Models;
 using System.Configuration;
-using DotEnv.;
+using DotNetEnv;
+using Newtonsoft.Json;
 
 namespace Jusibe.Tests
 {
     public class SmsTest
     {
         [Fact]
-        public void Test1()
+        public void Sms_Gets_Sent()
         {
+            DotNetEnv.Env.Load("../../../.env");
             JusibeClient client = new JusibeClient(new SMSConfig() {
-                AccessToken = 
-            })
+                AccessToken = System.Environment.GetEnvironmentVariable("Jusibe_Token"),
+                PublicKey = System.Environment.GetEnvironmentVariable("Jusibe_Key")
+            });
+
+            var result = client.Send(new RequestModel() {
+                From = "Mykeels",
+                To = System.Environment.GetEnvironmentVariable("Phone"),
+                Message = "Hello World"
+            }).Result;
+
+            Console.WriteLine(JsonConvert.SerializeObject(result));
         }
     }
 }
