780d06486edd045aa169aa2bfd63b7ce2b2c379e
Ikechi Michael <mykehell123@gmail.com>
2018-04-14 18:07:04 +0100

    feat: add KeyToken model

---
+++ Jusibe/Models/KeyToken.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Collections.Generic;
+
+namespace Jusibe.Models
+{
+    public class KeyToken
+    {
+        public string Key { get; set; }
+
+        public string Token { get; set; }
+    }
+}
